124024a2a0a2259a69561d01e84b693df2d78055
WeededMuffin385 <zagoruiko.mikhail@gmail.com>
2025-08-19 18:29:23 +1000

    [INDEX] Improved adaptivity

--- assets/js/toggle_ticket_form.js
+++ assets/js/toggle_ticket_form.js
@@ -3,9 +3,9 @@ const buy_ticket_form = document.getElementById("buy_ticket_form");
 function toggle_ticket_form_buy () {
     console.log("pressed the button")
 
-    if (buy_ticket_form.style.display === 'block') {
+    if (buy_ticket_form.style.display === 'flex') {
         buy_ticket_form.style.display = 'none';
     } else {
-        buy_ticket_form.style.display = 'block';
+        buy_ticket_form.style.display = 'flex';
     }
 }
